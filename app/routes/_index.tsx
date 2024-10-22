8c3cbd2a0498e95a09e25673b328ef0d61b66f13
rishi0808 <rishi0808@hotmail.com>
2024-10-22 23:22:40 +0100

    good push

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,7 +5,7 @@ import { Chat } from '~/components/chat/Chat.client';
 import { Header } from '~/components/header/Header';
 
 export const meta: MetaFunction = () => {
-  return [{ title: 'Bolt' }, { name: 'description', content: 'Talk with Bolt, an AI assistant from StackBlitz' }];
+  return [{ title: 'Hephaestus' }, { name: 'description', content: 'Talk with Hephaestus, an AI assistant from StackBlitz' }];
 };
 
 export const loader = () => json({});
